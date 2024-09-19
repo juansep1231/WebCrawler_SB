0be41807ba338df00301ae97a9eeaa91f96b1bba
Juan Posso <juan.posso@epn.edu.ec>
2024-09-19 12:38:05 -0500

    feat: add endpoint to get all entry data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import { scrapeHackerNews } from './controller/scraper.ts';
+
 const app = express();
 app.use(cors());
+
+app.get('/entries', async (req, res) => {
+    const entries = await scrapeHackerNews();
+    res.json(entries);
+});
+  
 app.listen(3001, () => console.log('Server running on port 3001'));
